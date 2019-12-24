560cd510cdd3922f4c99578fa481b20267057d7e
lunchi0124 <44578630+lunchi0124@users.noreply.github.com>
2019-12-24 11:46:48 +0800

    Update app.py

--- app.py
+++ app.py
@@ -47,13 +47,57 @@ def callback():
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-
     msg = event.message.text
     _low_msg = msg.lower()
     
+    _token = msg.strip().split(" ")
+    _low_token = _token[0].lower()
     
-    line_bot_api.reply_message(event.reply_token, _low_msg)
+    # query THU courses
+    if '課程' in _token[0] or '課表' in _token[0]:
+        cls_list = getCls(_token[1])
+        for cls in cls_list:
+            _message = TextSendMessage(text=cls)	#reply course
+            line_bot_api.reply_message(event.reply_token, _message)
+#            line_bot_api.push_message(event.source.user_id, TextSendMessage(text='123'))
+    elif '空氣' in _token[0] or 'pm2' in _low_token:
+        # query PM2.5
+        for _site in pm_site:
+            if _site == _token[1]:
+                _message = TextSendMessage(text=pm_site[_site]) #reply pm2.5 for the site
+                line_bot_api.reply_message(event.reply_token, _message)
+                break;
+    elif '!h' in _token[0] or '!help' in _token[0]:
+        _message = TextSendMessage(text="請輸入:課程, 誠品, 空氣 + <關鍵字>")
+        line_bot_api.reply_message(event.reply_token, _message)
 
+def loadPMJson():
+    with urllib.request.urlopen("http://opendata2.epa.gov.tw/AQI.json") as url:
+        data = json.loads(url.read().decode())
+        for ele in data:
+            pm_site[ele['SiteName']] = ele['PM2.5']
+
+def getCls(cls_prefix):
+    ret_cls = []
+    urlstr = 'https://course.thu.edu.tw/search-result/107/1/'
+    postfix = '/all/all'
+    
+    qry_cls = urlstr + cls_prefix + postfix
+    
+    resp = requests.get(qry_cls)
+    resp.encoding = 'utf-8'
+    soup = BeautifulSoup(resp.text, 'lxml')
+    clsrooms = soup.select('table.aqua_table tbody tr')
+    for cls in clsrooms:
+        cls_info = cls.find_all('td')[1]
+        cls_name = cls_info.text.strip()
+        sub_url = 'https://course.thu.edu.tw' + cls_info.find('a')['href']
+        ret_cls.append(cls_name + " " + sub_url)
+        break
+#         ret_cls = ret_cls + sub_url + "\n"
+
+    return ret_cls
+        
 import os
 if __name__ == "__main__":
     
