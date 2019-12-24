1fad6a4991a61e2c6cc2ba7fe7d2ffa4dd14200b
lunchi0124 <44578630+lunchi0124@users.noreply.github.com>
2019-12-24 12:08:29 +0800

    Update app.py

--- app.py
+++ app.py
@@ -100,6 +100,7 @@ def getCls(cls_prefix):
         
 import os
 if __name__ == "__main__":
+    loadPMJson()
     
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
